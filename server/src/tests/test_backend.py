d59448b7f333645d0ad8c1fc487de5078e13a9d2
GabrielaRamos79 <gabyramos@me.com>
2024-05-13 11:32:53 +0200

    Pytest installation and added to requirements

---
+++ server/src/tests/test_backend.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_anthony():
+    assert 1==1
+
+# def suma(a, b):
+#     return a + b
+
+# def tet_suma():
+#     assert suma(2, 3) == 5
+#     assert suma (0, 0) == 0
+#     assert suma (-1, 1) == 0
