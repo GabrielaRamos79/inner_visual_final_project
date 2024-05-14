1a199b07c6e40df030f40e82b7a327388cbb123b
Elle <ellefullstack@gmail.com>
2024-05-14 15:22:15 +0200

    Team´s description

--- client/InnerVisual/src/components/team/Team.jsx
+++ client/InnerVisual/src/components/team/Team.jsx
@@ -12,34 +12,36 @@
 
 
 // src/components/Team.jsx
+// src/components/Team.jsx
 import React from 'react';
 import './team.css'; // Importa el archivo de estilos
 import Kike from '../../assets/img/Kike.png';
-import juan from '../../assets/img/juan.jpg';
-
-
-
+import Juan from '../../assets/img/juan.jpg';
 
 const Team = () => {
   return (
     <div className="team-container">
-      <h2 className="team-title">Quiénes Somos</h2>
+      <h2 className="team-title">QUIENES SOMOS</h2>
       <div className="team-members">
         <div className="team-member">
           <div className="member-photo-container">
+          <h3 className="member-name">Kike</h3>
             <img src={Kike} alt="Miembro 1" className="member-photo" />
-            <h3 className="member-name">Kike</h3>
           </div>
-          <p className="member-description">"Si quieres cambiar algo, hazlo desde dentro" Kike se lo tomó al pie de la letra y creó Movimiento interno, un proyecto para aportar un cambio positivo a la sociedad a través del deporte.
-
-Pasó varios años viviendo y entrenando en China, donde aprendió sobre meditación, chikung, artes marciales… y ahora se dedica al entrenamiento de deportistas a través de estas técnicas de arraigo oriental.</p>
+         
+          <p className="member-description">
+            "Si quieres cambiar algo, hazlo desde dentro" Kike se lo tomó al pie de la letra y creó Movimiento interno, un proyecto para aportar un cambio positivo a la sociedad a través del deporte. Pasó varios años viviendo y entrenando en China, donde aprendió sobre meditación, chikung, artes marciales… y ahora se dedica al entrenamiento de deportistas a través de estas técnicas de arraigo oriental.
+          </p>
         </div>
         <div className="team-member">
           <div className="member-photo-container">
-          <img src={juan} alt="Miembro 2" className="member-photo" />
-            <h3 className="member-name">Juan</h3>
+          <h3 className="member-name">Juan</h3>
+            <img src={Juan} alt="Miembro 2" className="member-photo" />
           </div>
-          <p className="member-description">Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2.Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2.Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2.Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2.</p>
+          
+          <p className="member-description">
+            Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2. Breve descripción del miembro 2.
+          </p>
         </div>
       </div>
     </div>
@@ -47,3 +49,4 @@ Pasó varios años viviendo y entrenando en China, donde aprendió sobre meditac
 };
 
 export default Team;
+
