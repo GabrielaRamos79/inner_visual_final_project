2581701812f71a8727c365f5550572c2f3ff3375
GabrielaRamos79 <gabyramos@me.com>
2024-05-14 12:48:18 +0200

    Carrousel added in component history

--- client/InnerVisual/src/components/history/History.jsx
+++ client/InnerVisual/src/components/history/History.jsx
@@ -1,11 +1,48 @@
 import './history.css'
+import Carousel from 'react-bootstrap/Carousel';
+import history from '../../assets/img/history.jpg';
+import history2 from '../../assets/img/history2.png';
+import history3 from '../../assets/img/history3.jpg';
 
 const History = () => {
   return (
     <>
     <p>Historia</p>
+    <UncontrolledExample />
     </>
   )
 }
 
-export default History
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className='history' src={history} alt='Terapeuta con niña paciente' />
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <Carousel.Caption>
+          <h3>Nos importa tu salud</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='history2' src={history2} alt='Terapeuta ayudando a mujer' />
+        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <Carousel.Caption>
+          <h3>Tratamiento individual</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='history3' src={history3} alt='Terapeuta haciendo ejercicio de rehabilitacion' />
+        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <Carousel.Caption>
+          <h3>Estamos para atenderte</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+export default History;
--- server/src/tests/services/test_user_client.py
+++ server/src/tests/services/test_user_client.py
@@ -1,4 +1,5 @@
 import pytest
+# import requests
 from src.services.user_client_service import UserClientService
 
 @pytest.fixture(scope='session')
@@ -12,6 +13,25 @@ def test_get_id_user(user_by_id):
     assert 'id_user' in user_by_id
     assert user_by_id['id_user'] == 5
 
+
+# def test_get_id_user_returns_expected_result():
+#     # Arrange
+#     id_user = 5
+#     expected_result = {
+#         "email": "mercedes@gmail.com",
+#         "id_user": 5,
+#         "name": "Mercedes",
+#         "password": "123",
+#         "phone": "123456789",
+#         "photo": "FotoMercedesClient",
+#         "surname": "Suarez",
+#         "user_typeFK": 2
+#     }
+#     # Act
+#     response = requests.get(f'http://127.0.0.1:5000/user_client/get_user/{id_user}')
+#     actual_result = response.json()
+#     # Assert
+#     assert actual_result == expected_result
 
 # @pytest.fixture(scope='session')
 # def user_client_service_id():
