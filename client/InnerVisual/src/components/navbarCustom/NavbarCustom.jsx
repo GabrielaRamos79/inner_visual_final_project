b98267b9658fdcdc8b6266d97c364194a08a0db4
GabrielaRamos79 <gabyramos@me.com>
2024-05-14 15:00:24 +0200

    Navbar

--- client/InnerVisual/src/components/navbarCustom/NavbarCustom.jsx
+++ client/InnerVisual/src/components/navbarCustom/NavbarCustom.jsx
@@ -1,10 +1,43 @@
 import './navbarCustom.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 const NavbarCustom = () => {
   return (
     <>
-    <h3>Navbar</h3>
+    <Navbar collapseOnSelect expand="lg" className="navbar-custom">
+      <Container>
+        <Navbar.Brand href="#home" className="navbar-brand-custom fs-4">Inner-Visuals</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features" className='nav-link-custom'>Home</Nav.Link>
+            <Nav.Link href="#pricing" className='nav-link-custom'>Cursos</Nav.Link>
+            <Nav.Link href="#pricing" className='nav-link-custom'>About</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets" className='nav-link-custom'>Área Personal</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" className='nav-link-custom'>
+              Login/Registro
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   )
 }
