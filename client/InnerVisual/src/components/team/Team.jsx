cfe23e889425811141014746991a4e5303ba54a5
Elle <ellefullstack@gmail.com>
2024-05-14 15:44:38 +0200

    The names are where they belong

--- client/InnerVisual/src/components/team/Team.jsx
+++ client/InnerVisual/src/components/team/Team.jsx
@@ -10,19 +10,20 @@ const Team = () => {
       <h2 className="team-title">QUIENES SOMOS</h2>
       <div className="team-members">
         <div className="team-member">
+        <h3 className="member-name">Kike</h3>
           <div className="member-photo-container">
-          <h3 className="member-name">Kike</h3>
             <img src={Kike} alt="Miembro 1" className="member-photo" />
           </div>
          
           <p className="member-description">
             "Si quieres cambiar algo, hazlo desde dentro" Kike se lo tomó al pie de la letra y creó Movimiento interno, un proyecto para aportar un cambio positivo a la sociedad a través del deporte. Pasó varios años viviendo y entrenando en China, donde aprendió sobre meditación, chikung, artes marciales… y ahora se dedica al entrenamiento de deportistas a través de estas técnicas de arraigo oriental.
           </p>
+
         </div>
         <div className="team-member">
+        <h3 className="member-name">Juan</h3>
           <div className="member-photo-container">
-          <h3 className="member-name">Juan</h3>
-            <img src={Juan} alt="Miembro 2" className="member-photo" />
+          <img src={Juan} alt="Miembro 2" className="member-photo" />
           </div>
           
           <p className="member-description">
