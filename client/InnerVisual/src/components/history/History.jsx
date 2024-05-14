c3484a67ae525811270122bb640ddcd0ea8b17f9
GabrielaRamos79 <gabyramos@me.com>
2024-05-14 13:27:49 +0200

    History complete

--- client/InnerVisual/src/components/history/History.jsx
+++ client/InnerVisual/src/components/history/History.jsx
@@ -7,20 +7,12 @@ import history3 from '../../assets/img/history3.jpg';
 const History = () => {
   return (
     <>
-    <p>Historia</p>
-    <UncontrolledExample />
-    </>
-  )
-}
-
-function UncontrolledExample() {
-  return (
-    <Carousel>
+      <Carousel>
       <Carousel.Item>
         <img className='history' src={history} alt='Terapeuta con niña paciente' />
         {/* <ExampleCarouselImage text="First slide" /> */}
         <Carousel.Caption>
-          <h3>Nos importa tu salud</h3>
+          <h3 className='caption-title'>Nos importa tu salud</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -28,7 +20,7 @@ function UncontrolledExample() {
         <img className='history2' src={history2} alt='Terapeuta ayudando a mujer' />
         {/* <ExampleCarouselImage text="Second slide" /> */}
         <Carousel.Caption>
-          <h3>Tratamiento individual</h3>
+          <h3 className='caption-title'>Tratamiento individual</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -36,13 +28,15 @@ function UncontrolledExample() {
         <img className='history3' src={history3} alt='Terapeuta haciendo ejercicio de rehabilitacion' />
         {/* <ExampleCarouselImage text="Third slide" /> */}
         <Carousel.Caption>
-          <h3>Estamos para atenderte</h3>
+          <h3 className='caption-title'>Estamos para atenderte</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-  );
+    </>
+  )
 }
+
 export default History;
