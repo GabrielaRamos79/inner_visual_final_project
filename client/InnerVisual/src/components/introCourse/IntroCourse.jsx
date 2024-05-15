858688ceddb7438b87a1706571db2bdd88dd079e
Elle <ellefullstack@gmail.com>
2024-05-15 15:15:51 +0200

    Intro Course on process

--- client/InnerVisual/src/components/introCourse/IntroCourse.jsx
+++ client/InnerVisual/src/components/introCourse/IntroCourse.jsx
@@ -1,9 +1,25 @@
 import './introCourse.css'
+import React from 'react'
 
 const IntroCourse = () => {
   return (
     <>
-    <p>Introducción al curso</p>
+      <div className="introCourse">
+        <h3 className="introCourse-title">INTRO</h3>
+        <div className="course">
+          <div className="course_video">
+            <div className="button"><button className='video'>VIDEO 1</button>
+              <p className="video_title">VIZUALIZACION 1 - INTRO </p>
+            </div>
+          </div>
+          <div className="course_video">
+            <div className="button"><button className='video'>VIDEO 2</button>
+              <p className="video_title">MÉTODO 1 - INTRO </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>Introducción al curso</p>
     </>
   )
 }
