3e133d3b4f94448e11834dfc9e5fcae71ef5d710
Analucena53 <lucenapuertasana53@gmail.com>
2024-05-15 12:45:49 +0200

    Getuser

--- client/InnerVisual/src/components/tableClients/TableClients.jsx
+++ client/InnerVisual/src/components/tableClients/TableClients.jsx
@@ -1,11 +1,42 @@
 import './tableClients.css'
+import {GetUserHandler} from '../../handler/GetUserHandler'
+import React, { useState, useEffect } from 'react';
 
 const TableClients = () => {
-  return (
-    <>
-    <p>Tabla de clientes</p>
-    </>
-  )
+    const [users, setUsers] = useState([]);
+    const fetchData = async () => {
+        const userData = await GetUserHandler.getUser();
+        setUsers(userData);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <table id="myTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Surname</th>
+                        <th>Email</th>
+                        <th>Teléfono</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.id_user}>
+                            <td>{user.name}</td>
+                            <td>{user.surname}</td>
+                            <td>{user.email}</td>
+                            <td>{user.phone}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default TableClients
---
+++ client/InnerVisual/src/handler/GetUserHandler.js
@@ -0,0 +1,10 @@
+import { GetUserService } from '../services/GetUserService'
+
+export const UserHandler = {
+
+    async getUser() {
+        let user = await GetUserService.getUser();
+        return user;
+    }
+}
+export default UserHandler;
