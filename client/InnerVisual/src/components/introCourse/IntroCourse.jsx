a8915f2d36a645cf08aef9d689e9222e7d3dd71e
Elle <ellefullstack@gmail.com>
2024-05-16 11:28:01 +0200

    Intro Course Component´s CSS

--- client/InnerVisual/src/components/introCourse/IntroCourse.jsx
+++ client/InnerVisual/src/components/introCourse/IntroCourse.jsx
@@ -8,12 +8,12 @@ const IntroCourse = () => {
         <h3 className="introCourse-title">INTRO</h3>
         <div className="course">
           <div className="course_video">
-            <div className="button"><button className='video'>VIDEO 1</button>
-              <p className="video_title">VIZUALIZACION 1 - INTRO </p>
+            <div className="button"><button className='video'>VÍDEO 1</button>
+              <p className="video_title">VIZUALIZACIÓN 1 - INTRO </p>
             </div>
           </div>
           <div className="course_video">
-            <div className="button"><button className='video'>VIDEO 2</button>
+            <div className="button"><button className='video'>VÍDEO 2</button>
               <p className="video_title">MÉTODO 1 - INTRO </p>
             </div>
           </div>
