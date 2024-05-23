988e76d14999ce0ecc1495a34761f134175db912
GabrielaRamos79 <gabyramos@me.com>
2024-05-23 11:37:30 +0200

    Benefit CSS almost done

--- client/InnerVisual/src/components/benefit/Benefit.jsx
+++ client/InnerVisual/src/components/benefit/Benefit.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
-import benefit1 from '../../assets/img/benefit1.jpg';
-import benefit2 from '../../assets/img/benefit2.jpg';
-import benefit3 from '../../assets/img/benefit3.jpeg';
-import benefit4 from '../../assets/img/benefit4.jpeg';
+import gallery from '../../assets/img/gallery (1).png'
 import './benefit.css'
 
 const Benefit = () => {
   return (
   <div className='benefitcontainer'>
-    <h2 className='titleBenefit'>BENEFICIOS</h2>
     <section className='sectionBenefit'>
-
-    <div className='textBenefit'>
-    <ul>
-      <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-      reprehenderit aut perferendis officiis.</li>
-      <li className='liBenefit'>Lorem ipsum dolor, Lorem, ipsum. Excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-      reprehenderit aut perferendis officiis.</li>
-      <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-      reprehenderit aut perferendis officiis.</li>
-    </ul>
-    </div>
-  
-    <div>
-    <img className='benefit1' src={benefit1} alt="mujeres terminando en clase de yoga " />
-    <img className='benefit2' src={benefit2} alt="persona mayor despues de hacer ejercicio" />
-    <img className='benefit3' src={benefit3} alt="mujer haciendo yoga en la montaña" />
-    <img className='benefit4' src={benefit4} alt="jugador de baloncesto saltando" />
-    </div>
-
-
+      <div className='textContainer'>
+        <h2 className='titleBenefit'>BENEFICIOS</h2>
+        <div className='textBenefit'>
+          <ul>
+            <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
+            reprehenderit aut perferendis officiis.</li>
+            <li className='liBenefit'>Lorem ipsum dolor, Lorem, ipsum. Excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
+            reprehenderit aut perferendis officiis.</li>
+            <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
+            reprehenderit aut perferendis officiis.</li>
+          </ul>
+        </div>
+      </div>
+      <div className='imageContainer'>
+        <img src={gallery} alt='gallery' className='imgBenefit' />
+      </div>
     </section>
   </div>
-  )
-}
+  );
+};
 
 export default Benefit
