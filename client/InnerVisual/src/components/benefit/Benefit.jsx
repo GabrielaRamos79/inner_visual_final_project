516b326eecfdf0f55c536cf360fda7ad6c991901
GabrielaRamos79 <gabyramos@me.com>
2024-05-23 09:03:24 +0200

    Benefit component modified

--- client/InnerVisual/src/components/benefit/Benefit.jsx
+++ client/InnerVisual/src/components/benefit/Benefit.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import benefit1 from '../../assets/img/benefit1.jpg';
 import benefit2 from '../../assets/img/benefit2.jpg';
+import benefit3 from '../../assets/img/benefit3.jpeg';
+import benefit4 from '../../assets/img/benefit4.jpeg';
 import './benefit.css'
 
 const Benefit = () => {
   return (
-    <>
-    <h2 className='titleBenefit'>Beneficios</h2>
+  <div className='benefitcontainer'>
+    <h2 className='titleBenefit'>BENEFICIOS</h2>
     <section className='sectionBenefit'>
-  
-    <div>
-    <img className='benefit1' src={benefit1} alt="mujer sonriente moño recogido" />
-    <img className='benefit2' src={benefit2} alt="mujer sonriente moño recogido" />
-    </div>
 
     <div className='textBenefit'>
     <ul>
@@ -24,8 +21,17 @@ const Benefit = () => {
       reprehenderit aut perferendis officiis.</li>
     </ul>
     </div>
+  
+    <div>
+    <img className='benefit1' src={benefit1} alt="mujeres terminando en clase de yoga " />
+    <img className='benefit2' src={benefit2} alt="persona mayor despues de hacer ejercicio" />
+    <img className='benefit3' src={benefit3} alt="mujer haciendo yoga en la montaña" />
+    <img className='benefit4' src={benefit4} alt="jugador de baloncesto saltando" />
+    </div>
+
+
     </section>
-    </>
+  </div>
   )
 }
 
