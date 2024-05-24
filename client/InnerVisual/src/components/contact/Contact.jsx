eabca89669022ce653f301c25bc6a4a26a848d61
Mercedes <jmercedes.suarez@gmail.com>
2024-05-24 12:23:23 +0200

    style contact and jobs

--- client/InnerVisual/src/components/contact/Contact.jsx
+++ client/InnerVisual/src/components/contact/Contact.jsx
@@ -81,103 +81,111 @@ const Contact = () => {
 
   return (
     <>
-      <div className='jobs-container-contact-with-us'>
+      <div className='container-contact-with-us'>
+         <div className='container-title-contact'>
+            <h2 className='titleJobs-container-contact-with-us'>CONTACTO</h2>
+         </div>
+             <div className="contact-form-contact-with-us">
+                  <form onSubmit={handleSubmit}>
+                      <div className="input-group-contact-row">
 
-        <h2 className='titleJobs-container-contact-with-us'>CONTACTO</h2>
-     
-         
-        <form onSubmit={handleSubmit}>
+                        <div className="input-group-name-contact-with-us">
+                          <label htmlFor="name-contact-with-us">Nombre:</label>
+                          <input
+                            type="text"
+                            id="name-contact-with-us"
+                            value={name}
+                            placeholder="Introduce tu nombre"
+                            onChange={(e) => setName(e.target.value)}
+                            onBlur={() => setNameTouched(true)}
+                            className={`input-field ${nameTouched && !name.trim() ? 'error' : ''}`}
+                          />
+                        </div>
 
-            <div className="contact-form-contact-with-us">
-              
-                  <div className="input-group-contact-row">
+                        <div className="input-group-surname-contact-with-us">
+                          <label htmlFor="surname-contact-with-us">Apellido:</label>
+                          <input
+                            type="text"
+                            id="surname-contact-with-us"
+                            value={surname}
+                            placeholder="Introduce tu apellido"
+                            onChange={(e) => setSurname(e.target.value)}
+                            onBlur={() => setSurnameTouched(true)}
+                            className={`input-field ${surnameTouched && !surname.trim() ? 'error' : ''}`}
+                          />
+                        </div>
 
-                    <div className="input-group-name-contact-with-us">
-                      <label htmlFor="name-contact-with-us">Nombre:</label>
-                      <input
-                        type="text"
-                        id="name-contact-with-us"
-                        value={name}
-                        placeholder="Introduce tu nombre"
-                        onChange={(e) => setName(e.target.value)}
-                        onBlur={() => setNameTouched(true)}
-                        className={`input-field ${nameTouched && !name.trim() ? 'error' : ''}`}
-                      />
-                    </div>
+                      </div>
 
-                    <div className="input-group-surname-contact-with-us">
-                      <label htmlFor="surname-contact-with-us">Apellido:</label>
-                      <input
-                        type="text"
-                        id="surname-contact-with-us"
-                        value={surname}
-                        placeholder="Introduce tu apellido"
-                        onChange={(e) => setSurname(e.target.value)}
-                        onBlur={() => setSurnameTouched(true)}
-                        className={`input-field ${surnameTouched && !surname.trim() ? 'error' : ''}`}
-                      />
-                    </div>
-                  </div>
+                      <div className="input-group-contact-row">
 
-                  <div className="input-group-contact-row">
+                        <div className="input-group-telephone-contact-with-us">
+                          <label htmlFor="telephone-contact-with-us">Teléfono:</label>
+                          <input
+                            type="tel"
+                            id="telephone-contact-with-us"
+                            value={telephone}
+                            placeholder="Introduce tu telféfono"
+                            onChange={(e) => setTelephone(e.target.value)}
+                            onBlur={() => setTelephoneTouched(true)}
+                            className={`input-field ${telephoneTouched && (!telephone.trim() || !telephoneRegex.test(telephone)) ? 'error' : ''}`}
+                          />
+                        </div>
 
-                    <div className="input-group-telephone-contact-with-us">
-                      <label htmlFor="telephone-contact-with-us">Teléfono:</label>
-                      <input
-                        type="tel"
-                        id="telephone-contact-with-us"
-                        value={telephone}
-                        placeholder="Introduce tu telféfono"
-                        onChange={(e) => setTelephone(e.target.value)}
-                        onBlur={() => setTelephoneTouched(true)}
-                        className={`input-field ${telephoneTouched && (!telephone.trim() || !telephoneRegex.test(telephone)) ? 'error' : ''}`}
-                      />
-                   </div>
+                        <div className="input-group-email-contact-with-us">
+                          <label htmlFor="email-contact-with-us">Correo Electrónico:</label>
+                          <input
+                            type="email"
+                            id="email-contact-with-us"
+                            value={email}
+                            placeholder="Introduce tu correo electrónico"
+                            onChange={(e) => setEmail(e.target.value)}
+                            onBlur={() => setEmailTouched(true)}
+                            className={`input-field half-width ${emailTouched && !emailRegex.test(email) ? 'error' : ''}`}
+                          />
+                        </div>
 
-                    <div className="input-group-email-contact-with-us">
-                      <label htmlFor="email-contact-with-us">Correo Electrónico:</label>
-                      <input
-                        type="email"
-                        id="email-contact-with-us"
-                        value={email}
-                        placeholder="Introduce tu correo electrónico"
-                        onChange={(e) => setEmail(e.target.value)}
-                        onBlur={() => setEmailTouched(true)}
-                        className={`input-field half-width ${emailTouched && !emailRegex.test(email) ? 'error' : ''}`}
-                      />
-                    </div>
-                  </div>
+                      </div>
 
-                  <div className="input-group-message-contact-with-us">
-                    <label htmlFor="message-contact-with-us">Mensaje:</label>
-                    <textarea
-                      id="message-contact-with-us"
-                      value={message}
-                      placeholder="Escribe tu consulta"
-                      onChange={(e) => setMessage(e.target.value)}
-                      onBlur={() => setMessageTouched(true)}
-                      className={`textarea-field ${messageTouched && message.length < 5 ? 'error' : ''}`}
-                    />
-                  </div>
-                  <div className="input-group-terms-contact-with-us">
-                    <div className="checkbox-label-container">
-                      <input
-                        type="checkbox"
-                        id="terms-contact-with-us"
-                        checked={acceptTerms}
-                        onChange={(e) => setAcceptTerms(e.target.checked)}
-                        onBlur={() => setAcceptTermsTouched(true)}
-                      />
-                      <label htmlFor="terms-contact-with-us">He leído y acepto los <a href="/terminos-y-condiciones" target="_blank" rel="noopener noreferrer">términos y condiciones</a></label>
-                    </div>
-                    <div className="submit-button-wrapper-contact-with-us">
-                      <button type="submit" className="submit-button-contact-with-us">Enviar Mensaje</button>
-                    </div>
-                  </div>
-              </div>
-          </form>     
-        </div>
-      </>
+                      <div className="input-group-message-contact-with-us">
+                        <div className='input-message-contact'>
+                        <label htmlFor="message-contact-with-us">Mensaje:</label>
+                        </div>
+                        <div></div>
+                        <textarea
+                          id="message-contact-with-us"
+                          value={message}
+                          placeholder="Escribe tu consulta"
+                          onChange={(e) => setMessage(e.target.value)}
+                          onBlur={() => setMessageTouched(true)}
+                          className={`textarea-field ${messageTouched && message.length < 5 ? 'error' : ''}`}
+                        />
+                      </div>
+
+                      <div className="input-group-terms-contact-with-us">
+                                       
+                        <div className="checkbox-label-container">
+                          <div className="checkbox-container">
+                              <input
+                                type="checkbox"
+                                id="terms-contact-with-us"
+                                checked={acceptTerms}
+                                onChange={(e) => setAcceptTerms(e.target.checked)}
+                                onBlur={() => setAcceptTermsTouched(true)}
+                              />
+                          </div>
+                          <label htmlFor="terms-contact-with-us">
+                            He leído y acepto los <a href="/terminos-y-condiciones" target="_blank" rel="noopener noreferrer">términos y condiciones</a></label>
+                       
+                          {/* <div className="submit-button-wrapper-contact-with-us"> */}
+                           <button type="submit" className="submit-button-contact-with-us">Enviar Mensaje</button>
+                          {/* </div> */}
+                        </div>  
+                      </div>                      
+                   </form>                                 
+             </div>
+         </div>
+    </>
   );
 };
 
