5a226642ce8633925ad96eaa8820f57275e83517
inesgallo <anainesgf@gmail.com>
2024-05-24 23:34:14 +0200

    changes to see

--- server/src/routes/user_content_route.py
+++ server/src/routes/user_content_route.py
@@ -1,26 +1,36 @@
 from flask import Blueprint, request, jsonify
 from src.services.user_content_service import UserContentService
 from src.models.user_content_model import User_content
-
 main = Blueprint('inner_visual_user_content_blueprint', __name__)
-
-
-@main.route('/get_user_content', methods = ['GET'])
-def get_user_content():
+@main.route('/get_user_content/<id>', methods = ['GET'])
+def get_user_content(id):
+    id_userFK = id
     print("Hola get, user_content_router")
-    get_user_content = UserContentService.get_user_content()
+    get_user_content = UserContentService.get_user_content(id_userFK)
     print('Estoy dentro de metodo get_user_content')
     return jsonify(get_user_content)
-
-@main.route('/post_user_content', methods = ['POST'])
+@main.route('/post_user_content', methods=['POST'])
 def post_user_content():
     print("Hola post, user_content_router")
-
-    userFK = request.json['userFK']
-    contentFK = request.json['contentFK']
-    status_video = request.json['status_video']
-
-    user_content_table = User_content(0, userFK, contentFK, status_video)
-    post_user_content = UserContentService.post_user_content(user_content_table)
-    print(post_user_content)
-    return 'Registro exitoso'
+    try:
+        userFK = request.json['id_userFK']
+        post_user_content = UserContentService.post_user_content(userFK)
+        print(post_user_content)
+        return jsonify({"message": "Registro exitoso"}), 200
+    except Exception as ex:
+        print(ex)
+        return jsonify({"error": str(ex)}), 500
+@main.route('/update_user_content/<id_user>/<id_content>', methods = ['PATCH'])
+def updade_status_video(id_user, id_content):
+    print("Hola patch, user_router")
+    try:
+        userFK = id_user
+        contentFK = id_content
+        # status_video = request.json['status_video']
+        #user_content_table = User_content(None, userFK, contentFK, status_video, None)
+        patch_user_content = UserContentService.patch_user_content(userFK, contentFK)
+        print(patch_user_content)
+        return jsonify({"message": "Registro exitoso"}), 200
+    except Exception as ex:
+        print(ex)
+        return jsonify({"error": str(ex)}), 500
--- server/src/services/user_content_service.py
+++ server/src/services/user_content_service.py
@@ -1,7 +1,5 @@
 from src.database.db_mysql import get_connection
 from src.models.user_content_model import User_content
-
-
 class UserContentService():
     @classmethod
     def get_user_content(cls):
@@ -13,24 +11,16 @@ class UserContentService():
                 cursor.callproc('sp_get_user_content')
                 result = cursor.fetchall()
                 print(result)
-
-            users_content_json = [{"id_user_content": row[0], "id_user": row[1], "name": row[2], "surname": row[3], "id_content": row[4],  "title_video": row[5], "status_video": row[6]} for row in result]
+            users_content_json = [{"id_user_content": row[0], "id_user": row[1], "name": row[2], "surname": row[3], "id_content": row[4],  "title_video": row[5], "pdf": row[6], "url_video": row[7], "description": row[8], "status_video": row[9], "notes":[10]} for row in result]
             connection.close()
             return users_content_json
         except Exception as ex:
             print(ex)
-
     @classmethod
     def post_user_content(cls, user_content_table:User_content):
         try:
             connection  = get_connection()
             print(connection)
-            
-            userFK = user_content_table.userFK
-            contentFK = user_content_table.contentFK
-            status_video = user_content_table.status_video
-            
-                 
             with connection.cursor() as cursor:
                 cursor.callproc('sp_post_user_content', (userFK,contentFK,status_video))
                 connection.commit()
@@ -39,4 +29,19 @@ class UserContentService():
             return "Data base user_contentis close"
         except Exception as ex:
             print(ex)
-                       
+            return str(ex)
+    @classmethod
+    def patch_user_content(cls, userFK, contentFK):
+        try:
+            connection  = get_connection()
+            # userFK = user_content_table.userFK
+            # contentFK = user_content_table.contentFK
+            # status_video = user_content_table.status_video
+            with connection.cursor() as cursor:
+                cursor.callproc('sp_update_status_video', (userFK,contentFK))
+                connection.commit()
+                print('User updated successfully')
+            connection.close()
+            return "Data base is close"
+        except Exception as ex:
+            print(ex)
