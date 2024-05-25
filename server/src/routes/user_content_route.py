2e566bd42bddba44686eaf0b880ec9c1bf4a4adb
Yuliia <you@example.com>
2024-05-25 22:47:01 +0200

    User_content_route

--- server/src/routes/user_content_route.py
+++ server/src/routes/user_content_route.py
@@ -1,17 +1,22 @@
 from flask import Blueprint, request, jsonify
 from src.services.user_content_service import UserContentService
 from src.models.user_content_model import User_content
+
 main = Blueprint('inner_visual_user_content_blueprint', __name__)
+
+
 @main.route('/get_user_content/<id>', methods = ['GET'])
 def get_user_content(id):
-    id_userFK = id
+    id_userFK = id 
     print("Hola get, user_content_router")
     get_user_content = UserContentService.get_user_content(id_userFK)
     print('Estoy dentro de metodo get_user_content')
     return jsonify(get_user_content)
+
 @main.route('/post_user_content', methods=['POST'])
 def post_user_content():
     print("Hola post, user_content_router")
+
     try:
         userFK = request.json['id_userFK']
         post_user_content = UserContentService.post_user_content(userFK)
@@ -20,13 +25,17 @@ def post_user_content():
     except Exception as ex:
         print(ex)
         return jsonify({"error": str(ex)}), 500
+
+
 @main.route('/update_user_content/<id_user>/<id_content>', methods = ['PATCH'])
 def updade_status_video(id_user, id_content):
     print("Hola patch, user_router")
     try:
         userFK = id_user
         contentFK = id_content
-        # status_video = request.json['status_video']
+        # status_video = request.json['status_video'] 
+
+    
         #user_content_table = User_content(None, userFK, contentFK, status_video, None)
         patch_user_content = UserContentService.patch_user_content(userFK, contentFK)
         print(patch_user_content)
