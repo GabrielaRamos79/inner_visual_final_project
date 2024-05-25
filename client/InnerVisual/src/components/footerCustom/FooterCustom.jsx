7eacc79fdc8b3c387cd8055c8ba65bbfcd46f5bd
Analucena53 <lucenapuertasana53@gmail.com>
2024-05-25 03:38:56 +0200

    FooterCustom complete + buttom Conócenos working

--- client/InnerVisual/src/components/footerCustom/FooterCustom.jsx
+++ client/InnerVisual/src/components/footerCustom/FooterCustom.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
-import './footerCustom.css'
-import facebook from '../../assets/img/icons/facebooklogo.svg'
-import instagram from '../../assets/img/icons/instagramlogo.svg'
-import linkedin from '../../assets/img/icons/linkedinlogo.svg'
-import whatsapp from '../../assets/img/icons/whatsapplogo.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footerCustom.css';
+import facebook from '../../assets/img/icons/facebook.svg';
+import instagram from '../../assets/img/icons/instagram.svg';
+import linkedin from '../../assets/img/icons/linkedin.svg';
+import whatsapp from '../../assets/img/icons/whatsapp.svg';
+import logoFooter from '../../assets/img/logoFooter.svg';
 
 const FooterCustom = () => {
   return (
     <>
       <footer className='footer'>
-        <div className='redesFooter'>
-          <a href="https://www.facebook.com" target="_blank">
-            <img className='imgFooter' src={facebook} alt="logo de facebook" />
-          </a>
-          <a href="https://www.instagram.com" target="_blank">
-            <img className='imgFooter' src={instagram} alt="logo de instagram" />
-          </a>
-          <a href="https://www.linkedin.com" target="_blank">
-            <img className='imgFooter' src={linkedin} alt="logo de linkedin" />
-          </a>
-          <a href="https://www.whatsapp.com" target="_blank">
-            <img className='imgWhatsapp' src={whatsapp} alt="logo de whatsapp" />
-          </a>
+        <div>
+          <img className='imgLogo' src={logoFooter} alt="logo InnerVisual" />
         </div>
 
-        <div className='textFooter'>
-          <p className='copy'>Inner Visuals Copyright ©2024 | Todos los derechos reservados</p>
-          <p className='politic'>Aviso Legal - Política de Cookies - Política de Privacidad</p>
+        <div className='groupFooter'>
+          <div className='navFooter'>
+            <div className="var">
+              <div className="vr"></div>
+            </div>
+            <div className='leftFooter'>
+              <Link to="/" className='homeFooter'>Home</Link>
+              <Link to="/about" className='aboutFooter'>About us</Link>
+              <Link to="/#contact" className='contactFooter'>Contacto</Link> 
+            </div>
+          </div>
+          <div className='textFooter'>
+            <div className="var">
+              <div className="vr"></div>
+            </div>
+            <div className='rightFooter'>
+              <p className='politicFooter'>Política de privacidad</p>
+              <p className='cookiesFooter'>Política de cookies</p>
+              <p className='legalFooter'>Aviso legal</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='redesFooter'>
+          <div className='group1'>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <img className='imgFooter' src={facebook} alt="logo de facebook" />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <img className='imgFooter' src={instagram} alt="logo de instagram" />
+            </a>
+          </div>
+          <div className='group2'>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+              <img className='imgFooter' src={linkedin} alt="logo de linkedin" />
+            </a>
+            <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
+              <img className='imgFooter' src={whatsapp} alt="logo de whatsapp" />
+            </a>
+          </div>
         </div>
       </footer>
     </>
-  )
+  );
 }
 
-export default FooterCustom
+export default FooterCustom;
