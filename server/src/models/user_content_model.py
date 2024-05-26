cb5558640b05006b0000166dbe7231e0ac87353e
inesgallo <anainesgf@gmail.com>
2024-05-26 17:37:30 +0200

    updated notes server

--- server/src/models/user_content_model.py
+++ server/src/models/user_content_model.py
@@ -1,6 +1,7 @@
 class User_content():
-    def __init__(self, id_user_content, userFK, contentFK, status_video) -> None:
+    def _init_(self, id_user_content, userFK, contentFK, status_video, notes) -> None:
         self.id_user_content = id_user_content
         self.userFK = userFK
         self.contentFK = contentFK
         self.status_video = status_video
+        self.notes = notes
