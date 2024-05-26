905b40c8d45f05c0ec4a864267b34c96eb12741b
Yamila <yamilarobles10@gmail.com>
2024-05-26 07:13:00 +0200

    add text in benefit component

--- client/InnerVisual/src/components/benefit/Benefit.jsx
+++ client/InnerVisual/src/components/benefit/Benefit.jsx
@@ -10,12 +10,22 @@ const Benefit = () => {
         <h2 className='titleBenefit'>BENEFICIOS</h2>
         <div className='textBenefit'>
           <ul>
-            <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-            reprehenderit aut perferendis officiis.</li>
-            <li className='liBenefit'>Lorem ipsum dolor, Lorem, ipsum. Excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-            reprehenderit aut perferendis officiis.</li>
-            <li className='liBenefit'>Lorem ipsum dolor, excepturi eos iusto at animi sint eius, ratione ipsam accusamus blanditiis, neque voluptate.
-            reprehenderit aut perferendis officiis.</li>
+            <li className='liBenefit mb-5'> EL RITUAL ES ESA FORMACIÓN CREADA PARA TI, POR DEPORTISTAS QUE HAN SUFRIDO MENTALMENTE IGUAL QUE TÚ. 
+            EN SU INTERIOR ENCONTRARÁS 3 TÉCNICAS DE VISUALIZACIÓN Y MEDITACIÓN QUE TE AYUDARÁN A:</li>
+            <li className='liBenefit'>ESTAR MÁS CONCENTRADO</li>
+            <hr class="hr" />
+            <li className='liBenefit'>DESCONECTAR DEL RUIDO INNECESARIO DENTRO DE TU CABEZA</li>
+            <hr class="hr" />
+            <li className='liBenefit'>TRANSFORMAR TUS MIEDOS DUDAS E INNSEGURIDADES EN ENERGIA POSITIVA</li>
+            <hr class="hr" />
+            <li className="liBenefit">
+            CONSEGUIR ESA RELAJACIÓN FÍSICA Y MENTAL EN LA COMPETICIÓN QUE TE PERMITA HACER LO QUE TAN BIEN HACÍAS EN LOS ENTRENAMIENTOS
+            </li>
+            <hr class="hr" />
+            <li className="liBenefit">
+              Y SOBRE TODO COMENZAR A CONOCERTE MEJOR, MÁS ALLÁ DE OBJETIVOS Y METAS DEPORTIVAS
+            </li>
+            <hr class="hr" />
           </ul>
         </div>
       </div>
