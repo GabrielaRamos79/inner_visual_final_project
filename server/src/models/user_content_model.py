51d51c49944a777038a123f4bc73d23582ea7c39
Yuliia <you@example.com>
2024-05-27 16:29:58 +0200

    Change  _init_ for  __init__

--- server/src/models/user_content_model.py
+++ server/src/models/user_content_model.py
@@ -1,5 +1,5 @@
 class User_content():
-    def _init_(self, id_user_content, userFK, contentFK, status_video, notes) -> None:
+    def __init__(self, id_user_content, userFK, contentFK, status_video, notes) -> None:
         self.id_user_content = id_user_content
         self.userFK = userFK
         self.contentFK = contentFK
