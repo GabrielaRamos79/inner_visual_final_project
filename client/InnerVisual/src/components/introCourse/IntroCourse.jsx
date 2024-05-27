376a5016c7ce5e08cb2bafd49350cf097bfdc0f7
Yuliia <you@example.com>
2024-05-27 18:59:47 +0200

    Add videos into component IntroCourse

--- client/InnerVisual/src/components/introCourse/IntroCourse.jsx
+++ client/InnerVisual/src/components/introCourse/IntroCourse.jsx
@@ -1,6 +1,6 @@
 import "./introCourse.css";
 import React from "react";
-
+import ReactPlayer from 'react-player';
 
 const IntroCourse = () => {
   return (
@@ -10,12 +10,31 @@ const IntroCourse = () => {
           <h2 className="introCourse-title">INTRO</h2>
           <div className="course">
             <div className="course_video">
-              <button className="video">VÍDEO 1</button>
-              <p className="video_title">VIZUALIZACIÓN 1 - INTRO </p>
+              <button className="video">
+                <ReactPlayer
+                  className="player"
+                  url="https://youtu.be/ua6mkxO9GxQ?si=nYoHqf4C6fdeWBP2"
+                  controls={true}
+                  width="100%"
+                  height="99%"
+                />
+                VISUALIZACIÓN 1 - INTRODUCCIÓN A LA MEDITACIÓN
+              </button>
+              {/* <p className="video_title">VISUALIZACIÓN 1 - INTRODUCCIÓN A LA MEDITACIÓN </p> */}
             </div>
             <div className="course_video">
-              <button className="video">VÍDEO 2</button>
-              <p className="video_title">MÉTODO 1 - INTRO </p>
+
+              <button className="video">
+                <ReactPlayer
+                  className="player"
+                  url="https://youtu.be/57vkPyFsiUg?si=lE8YPsPuFdU5vt7T"
+                  controls={true}
+                  width="100%"
+                  height="99%"
+                />
+                VISUALIZACIÓN 2 - INTRODUCCIÓN A LA MEDITACIÓN DE PIE
+              </button>
+              {/* <p className="video_title">VISUALIZACIÓN 2 - INTRODUCCIÓN A LA MEDITACIÓN DE PIE </p> */}
             </div>
           </div>
         </section>
