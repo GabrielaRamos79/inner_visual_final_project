a53db56d7b68f391500339e634060a392936cc40
Analucena53 <lucenapuertasana53@gmail.com>
2024-05-27 12:16:57 +0200

    changes

--- client/InnerVisual/src/components/profileClient/ProfileClient.jsx
+++ client/InnerVisual/src/components/profileClient/ProfileClient.jsx
@@ -5,21 +5,21 @@ import fotoperfil from '../../assets/img/fotoperfil.jpg'
 const ProfileClient = () => {
   return (
     <>
-    <div className="profile-container ms-5">
-      <div className="welcome-message">
-        <h2>Bienvenido, [Nombre del Cliente]</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in nisl fringilla, at ultricies libero aliquet.</p>
+      <div className="profile-container ms-5">
+        <div className="welcome-message">
+          <h2>Bienvenido, [Nombre del Cliente]</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in nisl fringilla, at ultricies libero aliquet.</p>
+        </div>
+        <div className="action-buttons me-5">
+          <img className="profile-picture" src={fotoperfil} alt="Foto del Cliente" />
+          <button className='btn-custom' onClick={handleContinue}>Continuar Prácticas</button>
+          <button className='btn-custom' onClick={handleNotes}>Notas</button>
+          <button className='btn-custom' onClick={handleDelete}>Eliminar</button>
+        </div>
       </div>
-      <div className="action-buttons me-5">
-      <img className="profile-picture" src={fotoperfil} alt="Foto del Cliente" />
-        <button className='btn-custom' onClick={handleContinue}>Continuar Prácticas</button>
-        <button className='btn-custom' onClick={handleNotes}>Notas</button>
-        <button className='btn-custom' onClick={handleDelete}>Eliminar</button>
-      </div>
-    </div>
-    
 
-    <p>Perfil de cliente</p>
+
+      <p>Perfil de cliente</p>
     </>
   )
 };
